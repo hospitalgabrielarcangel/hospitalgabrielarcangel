b16b9eb9b09266a583f9e9b90c7edbf5de1a5d8c
hospitalgabrielarcangel <gabrielarcangel.dev@gmail.com>
2026-04-16 10:41:03 -0600

    add social nav to contact sidebar

--- src/components/layouts/contact-sidebar.tsx
+++ src/components/layouts/contact-sidebar.tsx
@@ -6,15 +6,19 @@ import { create } from "zustand"
 
 import { siteConfig } from "@/config/site"
 import {
+  cn,
   createCallUrl,
   createEmailUrl,
   createWhatsappUrl,
   formatPhoneNumber,
 } from "@/lib/utils"
+import { buttonVariants } from "@/lib/variants"
 import { Button } from "@/components/ui/button"
 import { ContactForm } from "@/components/forms/contact-form"
 import { Icons } from "@/components/icons"
 import Sidebar from "@/components/layouts/sidebar"
+
+import { SocialNav } from "../social-nav"
 
 interface ContactSidebarStore {
   isContactSidebarOpen: boolean
@@ -118,6 +122,108 @@ function ContactSidebar() {
                 {formatPhoneNumber(siteConfig.phoneNumbers.male)}
               </Button>
             </div>
+            <div className="flex items-center">
+              <Link
+                href={siteConfig.links.male.facebook}
+                target="_blank"
+                rel="noreferrer"
+                title="Facebook"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.facebook
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Facebook</span>
+              </Link>
+              <Link
+                href={siteConfig.links.male.instagram}
+                target="_blank"
+                rel="noreferrer"
+                title="Instagram"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.instagram
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Instagram</span>
+              </Link>
+              <Link
+                href={siteConfig.links.male.twitter}
+                target="_blank"
+                rel="noreferrer"
+                title="Twitter"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.twitter
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Twitter</span>
+              </Link>
+              <Link
+                href={siteConfig.links.male.youtube}
+                target="_blank"
+                rel="noreferrer"
+                title="Youtube"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.youtube
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Youtube</span>
+              </Link>
+              <Link
+                href={siteConfig.links.male.tiktok}
+                target="_blank"
+                rel="noreferrer"
+                title="Tiktok"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.tiktok
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Tiktok</span>
+              </Link>
+            </div>
           </div>
         </div>
         <div className="flex flex-col gap-y-12 border-t px-5 py-15 md:flex-row md:items-center md:justify-between md:gap-y-0">
@@ -179,6 +285,88 @@ function ContactSidebar() {
               >
                 {formatPhoneNumber(siteConfig.phoneNumbers.female)}
               </Button>
+            </div>
+            <div className="flex items-center">
+              <Link
+                href={siteConfig.links.female.facebook}
+                target="_blank"
+                rel="noreferrer"
+                title="Facebook"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.facebook
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Facebook</span>
+              </Link>
+              <Link
+                href={siteConfig.links.female.instagram}
+                target="_blank"
+                rel="noreferrer"
+                title="Instagram"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.instagram
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Instagram</span>
+              </Link>
+              <Link
+                href={siteConfig.links.female.twitter}
+                target="_blank"
+                rel="noreferrer"
+                title="Twitter"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.twitter
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Twitter</span>
+              </Link>
+              <Link
+                href={siteConfig.links.female.tiktok}
+                target="_blank"
+                rel="noreferrer"
+                title="Tiktok"
+                className={cn(
+                  buttonVariants({
+                    size: "icon",
+                    variant: "ghost",
+                    className: "group [&_svg:not([class*='size-'])]:size-6",
+                  }),
+                  "xs:flex hidden"
+                )}
+              >
+                <Icons.tiktok
+                  className="fill-muted-foreground group-hover:fill-foreground"
+                  aria-hidden
+                />
+                <span className="sr-only">Tiktok</span>
+              </Link>
             </div>
           </div>
         </div>
